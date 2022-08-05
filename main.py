6a71b62b27194bac7ca2b6edb8572028831bdac0
Gui <88752991+gvlk@users.noreply.github.com>
2022-08-04 23:03:59 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,170 @@
+# Função para inserir que deve chamar uma função de busca
+# Função para buscar
+# Função para imprimir as informações dos nodos
+# com opção de escolher a categoria de caminhamento (central, pré-fixado, pós-fixado (esquerda e direita)
+# Código que testa as funcionalidades
+
+class No:
+	def __init__(self, info):
+		self.info = info
+		self.esq = None
+		self.dir = None
+
+
+class ArvoreBinaria:
+	def __init__(self, nome='-----'):
+		self.nome = str(nome)
+		self.raiz = None
+		self.direcoes = {
+			'esq',
+			'dir'
+		}
+		self.caminhamentos = {
+			'c': None,
+			'pr': None,
+			'po': None
+		}
+		self.nomecaminhamentos = {
+			'c': 'central',
+			'pr': 'pré-fixado',
+			'po': 'pós-fixado'
+		}
+		self.nomedirecoes = {
+			'esq': 'esquerda',
+			'dir': 'direita'
+		}
+
+	def percorrer(self, func, caminhamento, direcao, func_dev=None):
+
+		def central(aux):
+			if aux is not None:
+				resultado = central(getattr(aux, direcao))
+				if resultado is not None:
+					return resultado
+
+				resultado = func(aux)
+				if resultado is not None:
+					return resultado
+
+				resultado = central(getattr(aux, direcao_oposta))
+				if resultado is not None:
+					return resultado
+			else:
+				return None
+
+		def prefixado(aux):
+			if aux is not None:
+				resultado = func(aux)
+				if resultado is not None:
+					return resultado
+
+				resultado = prefixado(getattr(aux, direcao))
+				if resultado is not None:
+					return resultado
+
+				resultado = prefixado(getattr(aux, direcao_oposta))
+				if resultado is not None:
+					return resultado
+			else:
+				return None
+
+		def posfixado(aux):
+			if aux is not None:
+				resultado = posfixado(getattr(aux, direcao))
+				if resultado is not None:
+					return resultado
+
+				resultado = posfixado(getattr(aux, direcao_oposta))
+				if resultado is not None:
+					return resultado
+
+				resultado = func(aux)
+				if resultado is not None:
+					return resultado
+			else:
+				return None
+
+		setattr(
+			self,
+			'caminhamentos',
+			{
+				'c': central,
+				'pr': prefixado,
+				'po': posfixado
+			}
+		)
+
+		if caminhamento in self.caminhamentos.keys():
+			if direcao in self.direcoes:
+				if func_dev is not None:
+					func_dev()
+				direcao_oposta = self.direcoes.copy()
+				direcao_oposta.discard(direcao)
+				direcao_oposta = direcao_oposta.pop()
+				return self.caminhamentos[caminhamento](self.raiz)
+
+			else:
+				raise ValueError(f"'{direcao}' Inválido. Parâmetro 'direcao' deve ser {self.direcoes}")
+		else:
+			raise ValueError(
+				f"'{caminhamento}' é inválido. Parâmetro 'caminhamento' deve ser {set(self.caminhamentos.keys())}")
+
+	def imprimir(self, caminhamento='pr', direcao='esq'):
+
+		def anuncio():
+			return print(
+				f"Impressão da árvore por caminhamento {self.nomecaminhamentos[caminhamento]} à {self.nomedirecoes[direcao]}"
+			)
+
+		def imprimir_wrapper(aux):
+			return print(aux.info, end=" ")
+
+		param = {
+			'func': imprimir_wrapper,
+			'func_dev': anuncio,
+			'caminhamento': caminhamento,
+			'direcao': direcao
+		}
+
+		return self.percorrer(**param)
+
+	def buscar(self, info_buscada, caminhamento='pr', direcao='esq'):
+
+		def comparar(aux):
+			if aux.info == info_buscada:
+				return aux
+			else:
+				return None
+
+		param = {
+			'func': comparar,
+			'caminhamento': caminhamento,
+			'direcao': direcao
+		}
+
+		return self.percorrer(**param)
+
+	def inserir(self, info, info_antecessor=None, direcao=None):
+		elemento = No(info)
+
+		if self.raiz is None:
+			self.raiz = elemento
+			print(f"Elemento {elemento.info} adicionado")
+
+		elif self.raiz is not None and info_antecessor is not None:
+			if direcao in self.direcoes:
+				elemento_antecessor = self.buscar(info_antecessor)
+				if elemento_antecessor is not None:
+					espaco_alvo = getattr(elemento_antecessor, direcao)
+					if espaco_alvo is None:
+						setattr(elemento_antecessor, direcao, elemento)
+						print(f"Elemento {elemento.info} adicionado")
+
+					else:
+						raise ValueError(f"Posição {self.nomedirecoes[direcao]} do nó '{info_antecessor} já ocupada'")
+				else:
+					raise ValueError(f"Nó '{info_antecessor}' não encontrado na árvore '{self.nome}'")
+			else:
+				raise ValueError(f"'{direcao}' é inválido. Parâmetro 'direcao' deve ser 'esq' ou 'dir'")
+		else:
+			raise ValueError("Parâmetro 'info_antecessor' é necessário")
---
+++ testes.py
@@ -0,0 +1,30 @@
+import main
+
+arvore_teste = main.ArvoreBinaria('arvore_teste')
+print('-'*80)
+arvore_teste.inserir('A')
+# Não é necessário passar os parâmetros 'info_antecessor' e 'direcao' ao inserir um primeiro Nó na árvore.
+arvore_teste.inserir('B', 'A', 'esq')
+arvore_teste.inserir('C', 'A', 'dir')
+arvore_teste.inserir('D', 'B', 'esq')
+arvore_teste.inserir('E', 'B', 'dir')
+arvore_teste.inserir('F', 'C', 'esq')
+arvore_teste.inserir('G', 'C', 'dir')
+arvore_teste.inserir('H', 'E', 'esq')
+arvore_teste.inserir('I', 'E', 'dir')
+arvore_teste.inserir('J', 'G', 'esq')
+# O objeto 'arvore_teste' é uma árvore encadeada com 4 níveis.
+print('-'*80)
+arvore_teste.imprimir()
+# O método 'imprimir' por padrão utiliza caminhamento pré-fixado à esquerda, porém ambas categorias de caminhamento e
+# direção podem ser determinadas utilizando os parâmetros da função. Para caminhamento: 'pr' = pré-fixado,
+# 'c' = central, 'po' = pós-fixado. Para direção: 'esq' = para esquerda, 'dir' = para direita.
+print()
+arvore_teste.imprimir('c', 'dir')
+print()
+arvore_teste.imprimir('po', 'dir')
+print()
+print('-'*80)
+# arvore_teste.inserir('P', 'M', 'esq')  # Exemplo de erro
+# arvore_teste.imprimir('pi', 'dir')     # Exemplo de erro
+# Caso parâmetros inválidos sejam passados às funções, erros vão notificar o problema.
